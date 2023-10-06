07cd67fe9ba9d2834f39b834afb5ac809e102fc5
Sebastiaobnj <junin_sbn@hotmail.com>
2023-10-06 02:18:22 -0300

    correção arquivo de testes

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -38,19 +38,19 @@ describe('Teste completo do projeto',  () => {
     expect(tableTag.querySelectorAll('tr')).toHaveLength(11)
   });
 
-  // test('Verificando filtro por texto', async () => {
-  //   render(
-  //   <DataProvider>
-  //     <App />
-  //   </DataProvider>);
-  //   const inputNameFIlter = screen.getByTestId(/name-filter/i);
-  //   await act(async () => {
-  //     userEvent.type(inputNameFIlter, 'oo');
-  //   })
-  //   await new Promise(resolve => setTimeout(resolve, 2000));
-  //   const tableTag = await screen.findByRole('table');
-  //   expect(tableTag.querySelectorAll('tr')).toHaveLength(3)
-  // });
+  test('Verificando filtro por texto', async () => {
+    render(
+    <DataProvider>
+      <App />
+    </DataProvider>);
+    const inputNameFIlter = screen.getByTestId(/name-filter/i);
+    await act(async () => {
+      userEvent.type(inputNameFIlter, 'oo');
+    })
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    const tableTag = await screen.findByRole('table');
+    expect(tableTag.querySelectorAll('tr')).toHaveLength(3)
+  });
 
   test('Verificando filtro maior que', async () => {
     render(
@@ -67,44 +67,6 @@ describe('Teste completo do projeto',  () => {
     await new Promise(resolve => setTimeout(resolve, 2000));
     const tableTag = await screen.findByRole('table');
     expect(tableTag.querySelectorAll('tr')).toHaveLength(7)
-  });
-
-  test('Verificando filtro menor que', async () => {
-    render(
-    <DataProvider>
-      <App />
-    </DataProvider>);
-    const inputFilter = screen.getByTestId(/value-filter/i);
-    const comparatorFilter = screen.getByTestId(/comparison-filter/i);
-    const buttonFilter = screen.getByTestId(/button-filter/i);
-
-    await act(async () => {
-      userEvent.type(inputFilter, '200000');
-      userEvent.selectOptions(comparatorFilter, 'menor que');
-      userEvent.click(buttonFilter);
-    })
-    await new Promise(resolve => setTimeout(resolve, 2000));
-    const tableTag = await screen.findByRole('table');
-    expect(tableTag.querySelectorAll('tr')).toHaveLength(2)
-  });
-
-  test('Verificando filtro igual a', async () => {
-    render(
-    <DataProvider>
-      <App />
-    </DataProvider>);
-    const inputFilter = screen.getByTestId(/value-filter/i);
-    const comparatorFilter = screen.getByTestId(/comparison-filter/i);
-    const buttonFilter = screen.getByTestId(/button-filter/i);
-
-    await act(async () => {
-      userEvent.type(inputFilter, '200000');
-      userEvent.selectOptions(comparatorFilter, 'igual a');
-      userEvent.click(buttonFilter);
-    })
-    await new Promise(resolve => setTimeout(resolve, 2000));
-    const tableTag = await screen.findByRole('table');
-    expect(tableTag.querySelectorAll('tr')).toHaveLength(2)
   });
 
   test('Verificando filtro igual a', async () => {
@@ -130,13 +92,13 @@ describe('Teste completo do projeto',  () => {
       userEvent.click(buttonFilter);
     })
     await new Promise(resolve => setTimeout(resolve, 2000));
-    const filtesEl = screen.getAllByTestId('filter');
-    console.log(filtesEl.length);
-    expect(filtesEl).toHaveLength(2);
+    const filtersEl = screen.getAllByTestId('filter');
+    console.log(filtersEl.length);
+    expect(filtersEl).toHaveLength(2);
     await act(async () => {
-      userEvent.click(filtesEl[0].querySelector('button')!);
+      userEvent.click(filtersEl[0].querySelector('button')!);
     })
-    const filtesEl2 = screen.getAllByTestId('filter');
-    expect(filtesEl2).toHaveLength(1);
+    const filtersEl2 = screen.getAllByTestId('filter');
+    expect(filtersEl2).toHaveLength(1);
   });
 });
