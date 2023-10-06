a1b35dbed14e50cdd286aa179a5fd49cbf74c68f
Sebastiaobnj <junin_sbn@hotmail.com>
2023-10-06 02:43:07 -0300

    adicionando comentarios em form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,6 +2,7 @@ import React, { useCallback, useEffect, useState } from 'react';
 import Table from './Table';
 import { FilterType } from '../types';
 
+// Valores iniciais para os filtros
 const INITIAL_FILTER_VALUES = {
   columnFilter: 'population',
   comparisonFilter: 'maior que',
@@ -9,22 +10,30 @@ const INITIAL_FILTER_VALUES = {
 };
 
 function Form() {
+  // State para o valor do filtro de texto
   const [inputFilter, setInputFilter] = useState('');
+
+  // State para os valores dos filtros numéricos
   const [filterValues, setFilterValues] = useState(INITIAL_FILTER_VALUES);
+
+  // State para manter a lista de filtros numéricos aplicados
   const [listNumericFilter, setListNumericFilter] = useState<FilterType[]>([]);
 
-  const optionsColumFilter = ['population', 'orbital_period',
-    'diameter', 'rotation_period', 'surface_water'];
+  // Opções de filtro por coluna
+  const optionsColumFilter = ['population',
+    'orbital_period', 'diameter', 'rotation_period', 'surface_water'];
 
+  // Opções de comparação
   const comparison = ['maior que', 'menor que', 'igual a'];
 
+  // Função para atualizar o valor do filtro de texto
   const handleInputFilter = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputFilter(event.target.value);
   };
 
-  const handleNumericFilter = (
-    event: React.ChangeEvent<HTMLSelectElement | HTMLInputElement>,
-  ) => {
+  // Função para atualizar os valores dos filtros numéricos
+  const handleNumericFilter = (event: React.
+    ChangeEvent<HTMLSelectElement | HTMLInputElement>) => {
     const { name, value } = event.target;
 
     setFilterValues((prev) => ({
@@ -33,13 +42,16 @@ function Form() {
     }));
   };
 
+  // Retorna as opções de filtro de coluna disponíveis
   const columnFilter = () => {
     if (listNumericFilter.length === 0) return optionsColumFilter;
 
-    return optionsColumFilter.filter((option) => (listNumericFilter
-      .some((filter) => filter.columnFilter !== option)));
+    return optionsColumFilter.filter((option) => (
+      listNumericFilter.some((filter) => filter.columnFilter !== option)
+    ));
   };
 
+  // Função para aplicar um novo filtro numérico
   const handleFilterClick = () => {
     const hasFilter = listNumericFilter.some((filter) => (
       filter.columnFilter === filterValues.columnFilter
@@ -50,12 +62,14 @@ function Form() {
     }
   };
 
+  // Função para remover um filtro numérico
   const handleRemoveFilter = (column: string) => {
     setListNumericFilter((prev) => prev
       .filter((filter) => filter.columnFilter !== column));
   };
 
-  const selectFistOption = useCallback(() => {
+  // Seleciona a primeira opção disponível para o filtro de coluna
+  const selectFirstOption = useCallback(() => {
     if (listNumericFilter.length === 0) {
       setFilterValues((prev) => ({
         ...prev,
@@ -64,21 +78,24 @@ function Form() {
       return;
     }
 
-    const column = optionsColumFilter.filter((option) => (listNumericFilter
-      .some((filter) => filter.columnFilter !== option)))[0];
+    const column = optionsColumFilter.filter((option) => (
+      listNumericFilter.some((filter) => filter.columnFilter !== option)
+    ))[0];
     setFilterValues((prev) => ({
       ...prev,
       columnFilter: column,
     }));
   }, [listNumericFilter]);
 
+  // Efeito colateral para selecionar a primeira opção disponível ao montar o componente
   useEffect(() => {
-    selectFistOption();
-  }, [selectFistOption]);
+    selectFirstOption();
+  }, [selectFirstOption]);
 
   return (
     <div>
       <section>
+        {/* Input para filtrar por texto */}
         <input
           onChange={ handleInputFilter }
           data-testid="name-filter"
@@ -86,6 +103,7 @@ function Form() {
         />
       </section>
       <section>
+        {/* Formulário de filtros numéricos */}
         <label htmlFor="columnFilter">
           Filter by column:
           <select
@@ -115,7 +133,7 @@ function Form() {
           </select>
         </label>
         <label htmlFor="valueFilter">
-          Valor:
+          Value:
           <input
             onChange={ (e) => handleNumericFilter(e) }
             value={ filterValues.valueFilter }
@@ -125,31 +143,34 @@ function Form() {
             type="text"
           />
         </label>
+        {/* Botão para aplicar o filtro */}
         <button
           onClick={ handleFilterClick }
           data-testid="button-filter"
         >
           Filtrar
-
         </button>
       </section>
       <section>
+        {/* Lista de filtros numéricos aplicados */}
         <ul>
           {listNumericFilter.map((filter) => (
             <li key={ filter.columnFilter } data-testid="filter">
               {`${filter.columnFilter} ${filter.comparisonFilter} ${filter.valueFilter}`}
+              {/* Botão para remover o filtro */}
               <button onClick={ () => handleRemoveFilter(filter.columnFilter) }>X</button>
             </li>
           ))}
+          {/* Botão para remover todos os filtros */}
           <button
             data-testid="button-remove-filters"
             onClick={ () => setListNumericFilter([]) }
           >
             Remover todas filtragens
-
           </button>
         </ul>
       </section>
+      {/* Componente de tabela que recebe os filtros */}
       <Table inputFilter={ inputFilter } listNumericFilter={ listNumericFilter } />
     </div>
   );
